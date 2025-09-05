aad059e1816a370dd9af1f0a3525c7ae649cf0bb
al2595 <al2595@cornell.edu>
2025-09-05 16:22:49 -0400

    finished implementation

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -15,10 +15,39 @@ module PairTripleDetector_GL
   output wire out
  );
 
+ wire w;
+ wire x;
+ wire y;
+
+ or( w, in0, in1);
+ and( x, in0, in1);
+ or( out, y, x);
+ and( y, w, in2);
+
   //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement pair/triple detector using explicit gate-level modeling
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+  task test_case_1_extensive();
+    t.test_case_begin( "test_case_1_extensive" );
+
+    //     in0 in1 in2 out
+    check( 0, 0, 0, 0 );
+    check( 0, 0, 1, 0 );
+    check( 0, 1, 0, 0 );
+    check( 0, 1, 1, 1 );
+    check( 1, 0, 0, 0 );
+    check( 1, 0, 1, 1 );
+    check( 1, 1, 0, 1 );
+    check( 1, 1, 1, 1 );
+
+
+
+    t.test_case_end();
+  endtask
+
+
+  
   `ECE2300_UNUSED( in0 );
   `ECE2300_UNUSED( in1 );
   `ECE2300_UNUSED( in2 );
